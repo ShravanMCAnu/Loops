ac7b441e8ed06a8db783b96208206307878c6fdf
bandi <bandi@LAPTOP-BJF2P1AA>
2022-10-28 16:30:00 +0530

    Add project files.

---
+++ Loops/Febnocii.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Febnocii
+    {
+        public  Febnocii(int n)
+        {
+            int a = 0, b = 1, t;
+            for(int i = 0; i <= n; i++)
+            {
+                t = a + b;
+                a = b;
+                b = t;
+                Console.WriteLine("Febnocii Series upto n: {0}   are.....", n, t);
+
+            }
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using Loops;
+using System;
+namespace Loops
+{
+    class Loops
+    {
+        public static void Main(String[] args)
+        {
+            Febnocii Fe = new(20);
+        }
+    }
+}
